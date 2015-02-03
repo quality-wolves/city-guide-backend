3c9d93ce9bffc4716cd17ff1828aecdcbe70f9b1
egor <eg0r.n1k0l43v@gmail.com>
2015-02-03 19:30:10 +0600

    added validations for models

--- app/models/hotspot.rb
+++ app/models/hotspot.rb
@@ -1,3 +1,11 @@
 class Hotspot < ActiveRecord::Base
   belongs_to :category
+  validates :name, :presence => true
+  validates :category, :presence => true
+
+  rails_admin do
+    configure :category do
+      label 'Category of this hotspot: '
+    end
+  end
 end
