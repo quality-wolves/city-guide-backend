561e43c5c37d7e6cb3df96d7e9287cf438577a8f
egor <eg0r.n1k0l43v@gmail.com>
2015-02-04 14:48:47 +0600

    configure images fields from hotspots

--- app/models/hotspot.rb
+++ app/models/hotspot.rb
@@ -1,5 +1,14 @@
 class Hotspot < ActiveRecord::Base
   validates :name, :presence => true
   validates :category, :presence => true
+
+  has_attached_file :banner, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :additionnal_image1, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :additionnal_image2, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+
+  validates_attachment_content_type :banner, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :additionnal_image1, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :additionnal_image2, :content_type => /\Aimage\/.*\Z/
+
   enum category: [ :stay, :eat, :buy, :drink, :see, :do]
 end
