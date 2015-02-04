a426f72d4efb5a04b05b8f94b501b9843fbb8287
polyakrecords <polyakrecords@gmail.com>
2015-02-04 19:06:12 +0600

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :hotspots
 
+  root :to => 'hotspots#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
