cf7cee6376bf675a217a96e660b2eb1a558b06c6
egor <eg0r.n1k0l43v@gmail.com>
2015-02-04 22:08:19 +0600

    styling create form

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150204152328) do
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "hotspots", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.integer  "category_id"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
     t.float    "lat"
     t.float    "lng"
+    t.text     "category"
     t.string   "banner_file_name"
     t.string   "banner_content_type"
     t.integer  "banner_file_size"
@@ -39,16 +33,10 @@ ActiveRecord::Schema.define(version: 20150204152328) do
     t.string   "aditionnal_image2_content_type"
     t.integer  "aditionnal_image2_file_size"
     t.datetime "aditionnal_image2_updated_at"
-<<<<<<< HEAD
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-=======
-    t.text     "category"
->>>>>>> c4ad62d6c62918e28c3157e3048ed3f2d3443ab3
   end
-
-  add_index "hotspots", ["category_id"], name: "index_hotspots_on_category_id"
 
 end
