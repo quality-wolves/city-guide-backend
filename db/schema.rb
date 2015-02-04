ca6e24aaa5e0b12f0f85d71a780b18fca9ed4b88
egor <eg0r.n1k0l43v@gmail.com>
2015-02-04 14:31:35 +0600

    add images field to hotspots

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203143239) do
+ActiveRecord::Schema.define(version: 20150204082250) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -23,10 +23,13 @@ ActiveRecord::Schema.define(version: 20150203143239) do
     t.string   "name"
     t.text     "description"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.float    "lat"
     t.float    "lng"
+    t.text     "banner"
+    t.text     "aditionnnal_image1"
+    t.text     "aditionnnal_image2"
   end
 
   add_index "hotspots", ["category_id"], name: "index_hotspots_on_category_id"
