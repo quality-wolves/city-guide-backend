7b32eba2224c7f6e25d7e9fa3585f8564f38035c
Alexander Khromov <alexander.khromov@gmail.com>
2015-02-15 21:04:47 +0600

    Add large images

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20150212190411) do
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "hotspots", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
