88abef1e57176a32b3bdf152ca82981b1bde140c
egor <eg0r.n1k0l43v@gmail.com>
2015-02-17 03:26:33 +0600

    add required validator to image on what`s on

--- app/models/whats_on.rb
+++ app/models/whats_on.rb
@@ -2,6 +2,7 @@ class WhatsOn < ActiveRecord::Base
 	include Paperclip::Glue
 
   validates :title, :presence => true
+  validates :image, :presence => true
   
   has_attached_file :image,
   					:styles => { :small => "100x100#", :medium => "275x275#", :large => "310x310#" },
