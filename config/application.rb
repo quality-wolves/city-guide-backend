93eb29070bc17ce10ea49cb1cdcc69783e3bb21b
egor <eg0r.n1k0l43v@gmail.com>
2015-02-18 18:56:32 +0600

    move archives path configuration to enviropmet

--- config/application.rb
+++ config/application.rb
@@ -23,13 +23,13 @@ module CityGuide
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.paperclip_defaults = {
-        :storage => :fog, 
-        :fog_credentials => {
-            :provider => "Local", 
-            :local_root => "#{Rails.root}/public"
-            }, 
-        :fog_directory => "", 
-        :fog_host => "localhost"
-        }
+      :storage => :fog, 
+      :fog_credentials => {
+        :provider => "Local", 
+        :local_root => "#{Rails.root}/public"
+      }, 
+      :fog_directory => "", 
+      :fog_host => "localhost"
+    }
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.configuration.archive_path = archive_path = File.join( Rails.root, 'archive')
+Rails.configuration.archive_db_path = archive_db_path = File.join( archive_path, 'db')
+
+Dir.mkdir archive_db_path unless Dir.exists? archive_db_path
