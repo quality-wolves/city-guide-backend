06938e5b54d3c96b5db58475c16db8d6a79a1745
egor <eg0r.n1k0l43v@gmail.com>
2015-03-26 06:18:10 +0600

    generate hotspot_image model

---
+++ app/models/hotspot_image.rb
@@ -0,0 +1,27 @@
+class HotspotImage < ActiveRecord::Base
+	belongs_to :hotspot
+
+#crop params accessors
+  # attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
+
+#banner iamge processing
+  #validates :banner, :presence => true
+
+	has_attached_file :file, 
+                    :styles => { :small => "100x100#", :medium => "275x275#", :large => '640x640#'},
+                    :path => 'uploads/:class-:id-:basename-:style.:extension'
+  	validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
+
+# validates_attachment_content_type :banner, :content_type => /\Aimage\/.*\Z/
+  
+# after_update :reprocess_banner, :if => :cropping?
+
+# def cropping?(param = nil)
+#   !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
+# end
+
+# def banner_geometry(style = :original)
+#   @geometry ||= {}
+#   @geometry[style] ||= Paperclip::Geometry.from_file(banner.path(style))
+# end
+end
