9f74dcfec4c3308ebfec024f74f50a5e0518d5a3
egor <eg0r.n1k0l43v@gmail.com>
2015-03-26 06:17:46 +0600

    apply migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324195221) do
+ActiveRecord::Schema.define(version: 20150325203420) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20150324195221) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "hotspot_images", force: :cascade do |t|
+    t.integer  "hotspot_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+  end
 
   create_table "hotspots", force: :cascade do |t|
     t.string   "name"
