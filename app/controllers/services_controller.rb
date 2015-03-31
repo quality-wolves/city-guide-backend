27ff1bf7b658a656e24943f3d5b0f8d97be88470
egor <eg0r.n1k0l43v@gmail.com>
2015-04-01 02:17:44 +0600

    fixed fetch attachments

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,67 +1,68 @@
 class ServicesController < ApplicationController
-	before_filter :authenticate
-	
-	def is_updated
-		@count = modified_hotspots.count
-	end
+  before_filter :authenticate
+  
+  def is_updated
+    modified_hotspots
+    @count = 0;
+    if(@last_date > @after_date)
+      @count = Hotspot.where(updated_at: @after_date..@last_date).count
+    end
+  end
 
-	def get_attachments_that_has_loaded_after
-		require 'rubygems'
-		require 'zip'
-		archive_path = Rails.configuration.archive_path
-		outputFile = File.join( archive_path, params[:date] + '_attachments.zip' )
+  def get_attachments_that_has_loaded_after
+    require 'rubygems'
+    require 'zip'
+    modified_hotspots
+    archive_path = Rails.configuration.archive_path
+    outputFile = File.join( archive_path, "attachments #{@after_date} <-> #{@last_date}.zip" )
 
-		unless File.exist?(outputFile)
-			io = Zip::File.open( outputFile, Zip::File::CREATE);
-	    modified_hotspots.each do |h|
-  			zipFilePath = h.hotspot_images[0].path(:large)
-	      io.get_output_stream( File.basename(zipFilePath) ) do |f| 
-	      	f.print( File.open( File.join( Rails.public_path, zipFilePath ) ,"rb" ).read() )
-	    	end
-	    end
-	    io.close();
-	    File.chmod 0644, outputFile 
-		end
+    unless File.exist?(outputFile)
+      io = Zip::File.open( outputFile, Zip::File::CREATE);
+      HotspotImage.where(file_updated_at: @after_date..@last_date).each do |h|
+        zipFilePath = h.file.path(:large)
+        io.get_output_stream( File.basename(zipFilePath) ) do |f| 
+          f.print( File.open( File.join( Rails.public_path, zipFilePath ) ,"rb" ).read() )
+        end
+      end
+      io.close();
+      File.chmod 0644, outputFile 
+    end
     
     send_file outputFile;
 
-	end
+  end
 
-	def get_database
-		send_file Dir[ File.join( Rails.configuration.archive_db_path, '*' ) ].max { |a,b| File.ctime(a) <=> File.ctime(b) }
-	end
+  def get_database
+    send_file Dir[ File.join( Rails.configuration.archive_db_path, '*' ) ].max { |a,b| File.ctime(a) <=> File.ctime(b) }
+  end
 
-	def publish_db
-		require 'rake'
-		CityGuide::Application.load_tasks
-		Rake::Task['zip:db_backup'].reenable
-		Rake::Task['zip:db_backup'].invoke
-		flash[:notice] = sprintf("Database is published now")
+  def publish_db
+    require 'rake'
+    CityGuide::Application.load_tasks
+    Rake::Task['zip:db_backup'].reenable
+    Rake::Task['zip:db_backup'].invoke
+    flash[:notice] = sprintf("Database is published now")
     redirect_to :back
-	end
+  end
 
-	protected
+  protected
 
-	def authenticate
-		if action_name == 'publish_db'
-			authenticate_admin! 
-		else
-	    authenticate_or_request_with_http_basic do |username, password|
-	      if( admin = Admin.find_by_email(username) )
-	      	admin.valid_password?(password)
-	      end
-	    end
-	  end
-	end
+  def authenticate
+    if action_name == 'publish_db'
+      authenticate_admin! 
+    else
+      authenticate_or_request_with_http_basic do |username, password|
+        if( admin = Admin.find_by_email(username) )
+          admin.valid_password?(password)
+        end
+      end
+    end
+  end
 
-	def modified_hotspots
-		after_date = DateTime.strptime(params[:date], '%Y-%m-%d %H:%M:%S') + 1.seconds
-		back_up = Dir[ File.join( Rails.configuration.archive_db_path, '*' ) ].max { |a,b| File.ctime(a) <=> File.ctime(b) }
-		last_date = File.ctime(back_up)
-		if(last_date < after_date)
-			return []
-		end
-		return Hotspot.where(updated_at: after_date..last_date)
-	end
+  def modified_hotspots
+    @after_date = (DateTime.strptime(params[:date], '%Y-%m-%d %H:%M:%S') + 1.seconds).strftime('%Y-%m-%d %H:%M:%S')
+    back_up = Dir[ File.join( Rails.configuration.archive_db_path, '*' ) ].max { |a,b| File.ctime(a) <=> File.ctime(b) }
+    @last_date = File.ctime(back_up).strftime('%Y-%m-%d %H:%M:%S')
+  end
 
 end
