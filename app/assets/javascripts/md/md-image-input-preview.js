9125f11ea28e85a834f318ec4518cc1a5dbeae4d
egor <eg0r.n1k0l43v@gmail.com>
2015-04-03 23:14:30 +0600

    add sortable for gallery

--- app/assets/javascripts/md/md-image-input-preview.js
+++ app/assets/javascripts/md/md-image-input-preview.js
@@ -1,4 +1,4 @@
-(function(){
+(function($){
   var W = function (jQ,options){
     W.super( this, 'constructor', jQ, options );
     this.$img = this.$.siblings( this.options.imageSelector );
@@ -9,7 +9,7 @@
   W.prototype.mkOnLoadImg = function(){
     var widget = this;
     return function(e){
-      var img = $('<img>',{src:e.target.result,width:'117px'});
+      var img = $('<img>',{src:e.target.result,width:'100%'});
       img.insertBefore(widget.$img);
       widget.$img.remove();
       widget.$img = img;
@@ -31,8 +31,46 @@
     if (w.files && w.files[0]) w.reader.readAsDataURL(w.files[0]);
   };
 
-  App.ready(function(){
-    $('.form-body').on('change','input[type="file"][role="image"]',W.onChangeFile);
+  var S = function(jQ){
+    S.super( this, 'constructor', jQ);
+    this.$position = this.$.find('input[name*="position"]');
+  }.extends( App.getWidgetsOfAlias('MDWidget') );
+
+  Object.defineProperty( S.prototype, 'position' , {
+    get:function(){
+      return parseInt(this.$position.val(),10);
+    },
+    set:function(p){
+      this.$position.val(p)
+    }
   });
 
-})();
+  S.sort = function(container){
+    container.children().map(S.setPosition);
+  }
+
+  S.setPosition = function(i){
+    var s = $.data(this,'s');
+    if(!s){
+      s = new S($(this));
+      $.data(this,'s',s);
+    }
+    s.position = i;
+  };
+
+  S.onAppend = function(item){
+    var $item = $(item), $p = $item.prev();
+    S.setPosition.call($item, !$p.length ? 0 : $p.data('s').position + 1);
+  };
+
+  App.ready(function(){
+    $('.gallery').on('change','input[type="file"][role="image"]',W.onChangeFile);
+    $('.gallery[type="sortable"] .row')
+      .sortable({ stop:function(e,ui){ S.sort(ui.item.parent()); } })
+      .each(function(){S.sort($(this));});
+    $('#nested-forms-list')
+      .on('cocoon:after-insert', function(e,item){ S.onAppend(item);})
+      .on('cocoon:before-remove', function(e,item){ S.sort(item.parent()); } );
+  });
+
+})(App.jQuery);
